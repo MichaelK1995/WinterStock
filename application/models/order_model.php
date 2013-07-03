f3e21329977e0de8d81dd267e844fcab4618559b
SpaceEmotion <spaceemotion@gmail.com>
2013-07-03 19:16:03 +0200

    Fix orders by user

--- application/models/order_model.php
+++ application/models/order_model.php
@@ -4,12 +4,18 @@ class Order_model extends CI_Model {
 
 	public function get_all_open() {
 		$this->db->select('orders.id, item, amount, user, requested, items.name');
-		$this->db->join('items', 'orders.item = items.id');
+		$this->join_items();
+		
 		return $this->db->get_where('orders', array('state' => 0))->result();
 	}
 
 	public function get_all_user($name) {
+		$this->join_items();
 		return $this->db->get_where('orders', array('user' => $name))->result();
+	}
+
+	private function join_items() {
+		$this->db->join('items', 'orders.item = items.id');
 	}
 
 }
